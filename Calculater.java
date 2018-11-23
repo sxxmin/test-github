97399d4b3a5aa0cfe346c74e72697de7eab8bff1
sxxmin <skeajd236@gmail.com>
2018-11-23 20:18:26 +0900

    testing calculater

---
+++ Calculater.java
@@ -0,0 +1,13 @@
+class Calculater{
+  public static void main(String argc[]) {
+    int a = 4;
+    int b = 3;
+    System.out.println();
+  }
+  public int sum(int a, int b) {
+    return a + b;
+  }
+  public int sub(int a, int b) {
+    return a - b;
+  }
+}
